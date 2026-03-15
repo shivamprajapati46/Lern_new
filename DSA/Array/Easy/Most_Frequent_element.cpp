0066bf0db78eaa31f2ec98d7cad7e96737523700
Shivamprajapati844505 <sp1172389@gmail.com>
2026-03-16 00:33:24 +0530

    hashing

--- DSA/Array/Easy/Most_Frequent_element.cpp
+++ DSA/Array/Easy/Most_Frequent_element.cpp
@@ -1,29 +1,57 @@
 #include<iostream>
+#include<unordered_map>
 using namespace std;
+
+// int main(){
+
+//    int n;
+//    cin>>n;
+//    int arr[n];
+//    for(int i=0; i<n; i++){
+//      cin>>arr[i];
+//    }
+
+//     int hash[100] = {0};
+
+//     for(int i=0;i<n;i++){
+//         hash[arr[i]]++;
+//     }
+
+//     int maxii = 0;
+
+//     for(int i=0;i<100;i++){
+//         if(hash[i] > maxii){
+//             maxii = hash[i];
+//         }
+//     }
+
+//     cout<<"Most frequent frequency = "<<maxii;
+//     return 0;
+// }
 
 int main(){
 
-   int n;
-   cin>>n;
-   int arr[n];
-   for(int i=0; i<n; i++){
-     cin>>arr[i];
-   }
-
-    int hash[100] = {0};
-
+    int n;
+    cin>>n;
+    int arr[n];
     for(int i=0;i<n;i++){
-        hash[arr[i]]++;
+        cin>>arr[i];
     }
 
-    int maxi = 0;
+    unordered_map<int,int>mpp;
 
-    for(int i=0;i<100;i++){
-        if(hash[i] > maxi){
-            maxi = hash[i];
+    for(int i=0; i<n; i++){
+        mpp[arr[i]]++;
+    }
+  int maxi= 0;
+
+    for(auto it:mpp){
+        if(it.second > maxi){
+            maxi=it.second;
         }
     }
-
-    cout<<"Most frequent frequency = "<<maxi;
+   cout<<"Most frequent frequency = "<<maxi;
     return 0;
 }
+
+
